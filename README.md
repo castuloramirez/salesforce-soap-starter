8ae4fb007fe2f9fb4cce9ebe860375f1739fe6fe
James Ward <james@jamesward.org>
2017-01-23 11:15:18 -0700

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+Salesforce SOAP Starter
+=======================
+
+A simple command line Java app that connects to the Salesforce SOAP APIs.
+
+Run on Mac or Linux:
+
+    ./mvnw compile exec:java
+
+Run on Windows:
+
+    mvnw compile exec:java
---
+++ src/main/java/com/example/SalesforceSoapStarter.java
@@ -0,0 +1,55 @@
+package com.example;
+
+import com.sforce.soap.partner.Connector;
+import com.sforce.soap.partner.LoginResult;
+import com.sforce.soap.partner.PartnerConnection;
+import com.sforce.soap.partner.fault.ApiFault;
+import com.sforce.ws.ConnectionException;
+import com.sforce.ws.ConnectorConfig;
+
+public class SalesforceSoapStarter {
+
+    private static final String LOGIN_URL =  "https://login.salesforce.com/services/Soap/u/38.0";
+
+    public static void main(String[] args) {
+
+        String username;
+        String password;
+
+        if (args.length == 2) {
+            username = args[0];
+            password = args[1];
+        }
+        else {
+            System.out.print("Salesforce Username: ");
+            username = System.console().readLine();
+
+            System.out.print("Salesforce Password: ");
+            password = new String(System.console().readPassword());
+        }
+
+        try {
+            final ConnectorConfig loginConfig = new ConnectorConfig();
+            loginConfig.setAuthEndpoint(LOGIN_URL);
+            loginConfig.setServiceEndpoint(LOGIN_URL);
+            loginConfig.setManualLogin(true);
+
+            final PartnerConnection partnerConnection = Connector.newConnection(loginConfig);
+            final LoginResult loginResult = partnerConnection.login(username, password);
+
+            partnerConnection.getConfig().setServiceEndpoint(loginResult.getServerUrl());
+            partnerConnection.setSessionHeader(loginResult.getSessionId());
+
+            System.out.println("Describing Contact SObject");
+            System.out.println(partnerConnection.describeSObject("Contact").toString());
+        }
+        catch (ApiFault e) {
+            System.out.println("Could not connect to Salesforce: " + e.getExceptionMessage());
+        }
+        catch (ConnectionException e) {
+            System.out.println("Could not connect to Salesforce");
+            e.printStackTrace();
+        }
+    }
+
+}
